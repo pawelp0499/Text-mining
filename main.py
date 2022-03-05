83ffcc2ee51f53299e3e3eb1953f1929648c291e
Pawel <pawel.pechta@edu.uekat.pl>
2022-03-05 19:56:54 +0100

    fix: renamed functions

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from zad1a import delete_numbers
 from zad1b import delete_html
 from zad1c import delete_punctuation_marks
-from zad2 import delete_hashtags
-from zad3 import delete_emoticons
+from zad2 import extract_hashtags
+from zad3 import extract_emoticons
 
 
 print(delete_numbers('Dzisiaj mamy 4 stopnie na plusie, 1 marca 2022 roku'))
@@ -16,18 +16,18 @@ print(delete_punctuation_marks("""Lorem ipsum dolor sit amet, consectetur;
 blandit, mi eu porta; lobortis, tortor nisl facilisis leo, at tristique augue
  risus eu risus. """))
 
-print(delete_hashtags("""Lorem ipsum dolor
+extract_hashtags("""Lorem ipsum dolor
 sit amet, consectetur adipiscing elit. Sed #texting eget mattis sem. 
 Mauris #frasista
 egestas erat #tweetext quam, ut faucibus eros #frasier congue et. In blandit,
  mi eu porta
 lobortis, tortor nisl facilisis leo, at tristique #frasistas augue risus
- eu risus."""))
+ eu risus.""")
 
-print(delete_emoticons("""Lorem ipsum dolor
+extract_emoticons("""Lorem ipsum dolor
  sit amet, consectetur :> adipiscing elit. Sed #texting eget :-) mattis sem.
   Mauris #frasista
 egestas erat #tweetext quam, ut faucibus eros ;) #frasier congue et. :)
  In blandit, mi eu porta
 lobortis, tortor nisl facilisis leo ;(, at tristique #frasistas augue risus
- :< eu risus. ;< """))
+ :< eu risus. ;< """)
