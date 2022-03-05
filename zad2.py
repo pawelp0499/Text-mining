0cc9b26fc75e0b8ef253351a41b0c5736f7d8d63
Pawel <pawel.pechta@edu.uekat.pl>
2022-03-05 19:51:30 +0100

    fix: function edits

--- zad2.py
+++ zad2.py
@@ -7,7 +7,6 @@ import re
 def extract_hashtags(text: str):
     """Enter a text as a string parameter and get
         the result as extracted hashtags"""
-    result = re.findall("#[^\s]+", text)
+    result = re.findall(r"#[^\s]+", text)
     for hashtag in result:
         print(hashtag)
-
