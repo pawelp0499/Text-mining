d2b30e5b71bdc6b493bdc69470c2310be1053d60
Pawel <pawel.pechta@edu.uekat.pl>
2022-03-08 09:08:33 +0100

    feat: added function to clear text

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-# This is a sample Python script
+from zad1 import clear_text
+
+
+print(clear_text("""<div>Lorem IPSUM dolor 11 sit amet, consectetur :>
+adipiscing elit.      Sed #texting eget :-) mattis sem. <a> Mauris #frasista 
+egestas erat #tweetext quam, ut faucibus eros ;) 12 #frasier </a> congue :) et. 
+In blandit,           mi eu porta lobortis, tortor nisl facilisis <b>leo</b> ;(,
+at tristique 15        #frasistas augue risus       :< eu risus. ;< </div> """))
