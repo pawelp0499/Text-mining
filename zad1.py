13a8b07e93036c1608020aea8cbf02467eaa1982
Pawel <pawel.pechta@edu.uekat.pl>
2022-03-12 16:32:29 +0100

    fix: emoticons extracting

--- zad1.py
+++ zad1.py
@@ -2,8 +2,9 @@ import re
 
 
 def clear_text(text: str) -> str:
-    emoticons = re.sub(r"[],[']", '', str(re.findall(r"[:;][)\-(<>]+", text)))
-    text_without_emoticons = re.sub(r"[:;][)\-(<>]+", '', text.lower())
+    emoticons = ' '.join([str(i) for i in
+                          re.findall(r"[:;][-]?[)\-(<>]", text)])
+    text_without_emoticons = re.sub(r"[:;][-]?[)\-(<>]", '', text.lower())
     no_digits = re.sub(r'\d', '', text_without_emoticons)
     no_html_tags = re.sub('<.*?>', '', no_digits)
     no_punctuation_marks = re.sub('[,;.]', '', no_html_tags)
