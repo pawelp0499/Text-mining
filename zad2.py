e814ba37afd1aa6c89296e1801d5b262bbac36e5
Pawel <pawel.pechta@edu.uekat.pl>
2022-03-12 16:21:25 +0100

    feat: added function to remove eng stopwords from text

--- zad2.py
+++ zad2.py
@@ -1,13 +1,11 @@
-import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-nltk.download('punkt')
 
 
 def remove_stop_words(text: str) -> str:
-    all_stopwords = stopwords.words('english')
+    eng_stopwords = stopwords.words('english')
 
-    text_tokens = word_tokenize(text)
-    tokens_without_sw = [word for word in text_tokens if
-                         not word in all_stopwords]
-    return tokens_without_sw
+    token = word_tokenize(text)
+    tokens_without_sw = [word for word in token if
+                         word not in eng_stopwords]
+    return ' '.join([str(i) for i in tokens_without_sw])
