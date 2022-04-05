f893a356ac7f730b048867a44f487c49a84d5562
Pawel <pawel.pechta@edu.uekat.pl>
2022-04-05 09:33:54 +0200

    display top 10 tokens

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from services.text_tokenizer import text_tokenizer
 import pandas as pd
+import numpy as np
 
 
 dataset = pd.read_csv('./data/True.csv', usecols=['title', 'text'])
-sample = dataset['title'].sample(10)
+sample = dataset['title'][:10]
 
 vectorizer = CountVectorizer(tokenizer=text_tokenizer)
 # vectorizer = TfidfVectorizer(tokenizer=text_tokenizer)
 
-x_transform_sample = vectorizer.fit_transform(sample)
-print(x_transform_sample.toarray())
+X_transform_sample = vectorizer.fit_transform(sample)
+print(vectorizer.get_feature_names_out())
+titles = (vectorizer.get_feature_names_out())
+array = X_transform_sample.toarray()
+
+"""
+Display top 10 tokens
+"""
+column_sum = np.sum(array, axis=0)
+max_val = np.argpartition(column_sum, -10)[-10:]
+top_10 = column_sum[max_val]
+print(top_10)
+print(titles[np.argpartition(column_sum, -10)[-10:]])
