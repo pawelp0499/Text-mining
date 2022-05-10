9c6f52c412770f0124a4757a295dffbc894e8250
Pawel <pawel.pechta@edu.uekat.pl>
2022-05-10 08:23:17 +0200

    feat: add services

---
+++ services/clear_text.py
@@ -0,0 +1,13 @@
+import re
+
+
+def clear_text(text: str) -> str:
+    emoticons = ' '.join([str(i) for i in
+                          re.findall(r"[:;][-]?[)\-(<>]", text)])
+    text_without_emoticons = re.sub(r"[:;][-]?[)\-(<>]", '', text.lower())
+    no_digits = re.sub(r'\d', '', text_without_emoticons)
+    no_html_tags = re.sub('<.*?>', '', no_digits)
+    no_punctuation_marks = re.sub('[,;.]', '', no_html_tags)
+    no_multiple_spaces = re.sub(' +', ' ', no_punctuation_marks)
+    result = no_multiple_spaces + emoticons
+    return result
