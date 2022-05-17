d5fb074db265838d9a3e9bafdcad3571ddc50bc9
Pawel <pawel.pechta@edu.uekat.pl>
2022-05-18 00:28:04 +0200

    feat: calculation of distance and similarity

--- main.py
+++ main.py
@@ -1,55 +1,20 @@
+from sklearn.metrics.pairwise import euclidean_distances, cosine_distances,\
+    cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 from services.text_tokenizer import text_tokenizer
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
-from sklearn.ensemble import RandomForestClassifier
 
 
-true = pd.read_csv('./data/True.csv', usecols=['title', 'text'])
-true['type'] = 'True'
-
-fake = pd.read_csv('./data/Fake.csv', usecols=['title', 'text'])
-fake['type'] = 'Fake'
-
-frames = [true, fake]
-result = pd.concat(frames)
-
-y = result['type']
-X = result['title']
-
-X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                    test_size=0.3,
-                                                    random_state=0)
+data = pd.read_csv('./data/dataset.csv')
+print(data)
 
 vectorizer = CountVectorizer(tokenizer=text_tokenizer)
 
-X_train = vectorizer.fit_transform(X_train)
-X_test = vectorizer.transform(X_test)
+X_transform = vectorizer.fit_transform(data['TeamName'])
+array = X_transform.toarray()
 
-print(X_train.shape)
+df = pd.DataFrame(array, columns=vectorizer.get_feature_names_out())
 
-# Decision Tree
-
-clf = DecisionTreeClassifier()
-
-clf = clf.fit(X_train, y_train)
-
-y_pred_1 = clf.predict(X_test)
-
-print("Accuracy:", metrics.accuracy_score(y_test, y_pred_1))
-print("Accuracy:", metrics.classification_report(y_test, y_pred_1))
-
-'''
-# Random forest
-
-crf = RandomForestClassifier(n_estimators=100)
-
-crf.fit(X_train, y_train)
-
-y_pred_2 = crf.predict(X_test)
-
-print("Accuracy:", metrics.accuracy_score(y_test, y_pred_2))
-print("Accuracy:", metrics.classification_report(y_test, y_pred_2))
-'''
+print(f"Euclidean distance:\n {euclidean_distances(df,df)}")
+print(f"Cosine distance:\n {cosine_distances(df,df)}")
+print(f"Cosine similarity:\n {cosine_similarity(df,df)}")
